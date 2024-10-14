8b7a98422b9e55dbb4d87db9333e64adc989010a
Pulkit Chopra <pulkitchopra690@gmail.com>
2024-10-14 10:43:14 +0530

    small changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,16 +11,16 @@ const Home = () => {
       <BannerProduct/>
 
       <HorizontalCardProduct category={"electronics"} heading={"Top's Electronics"}/>
-      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+      <HorizontalCardProduct category={"fashion"} heading={"Popular Fashion"}/>
 
-      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
-      <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
-      <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
-      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
-      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
+      <VerticalCardProduct category={"personalcare"} heading={"Personal Care"}/>
+      <VerticalCardProduct category={"offocesupplies"} heading={"Office Supplies"}/>
+      <VerticalCardProduct category={"beauty"} heading={"Beauty"}/>
+      <VerticalCardProduct category={"furniture"} heading={"Furniture"}/>
+      {/* <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
       <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
       <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/> */}
     </div>
   )
 }
